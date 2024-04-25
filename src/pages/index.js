389ae547275f2e0b0aef21d40b4bf5391ea5e1c3
ssson <ssson@tigrison.com>
2024-04-25 16:13:14 +0900

    Finished Gatsby Tutorial Part 6

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 // Step 1: Import React
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
 import Seo from '../components/seo'
